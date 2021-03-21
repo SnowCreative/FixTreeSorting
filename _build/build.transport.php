1425584869013d820a9e34cd5c0f8b1b59106a3c
SnowCreative <2528374+SnowCreative@users.noreply.github.com>
2021-03-21 16:57:35 -0400

    sdf

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -1,36 +1,9 @@
 <?php
-/**
-* FixTreeSorting
-*
-* Copyright 2021 by Michael SNow <modx@snowcreative.com>
-*
-* FixTreeSorting is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the Free
-* Software Foundation; either version 2 of the License, or (at your option) any
-* later version.
-*
-* FixTreeSorting is distributed in the hope that it will be useful, but WITHOUT ANY
-* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with
-* FixTreeSorting; if not, write to the Free Software Foundation, Inc., 59 Temple
-* Place, Suite 330, Boston, MA 02111-1307 USA
-*
-* @package fixtreesorting
-*/
-/**
-* FixTreeSorting build script
-*
-* @package fixtreesorting
-* @subpackage build
-*/
-$mtime = microtime();
-$mtime = explode(" ", $mtime);
-$mtime = $mtime[1] + $mtime[0];
-$tstart = $mtime;
+$tstart = explode(' ', microtime());
+$tstart = $tstart[1] + $tstart[0];
 set_time_limit(0);
 
+/* define package names */
 define('PKG_NAME','FixTreeSorting');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
 define('PKG_VERSION','1.0');
@@ -42,38 +15,30 @@ $sources= array (
 	'build' => $root .'_build/',
 	'resolvers' => $root . '_build/resolvers/',
 	'data' => $root . '_build/data/',
+	'elements' => $root.'core/components/'.PKG_NAME_LOWER.'/elements/',
 	'source_core' => $root.'core/components/fixtreesorting',
 	'docs' => $root.'core/components/fixtreesorting/docs/',
 );
 unset($root);
 
 /* override with your own defines here (see build.config.sample.php) */
-require_once dirname(__FILE__) . '/build.config.php';
+require_once $sources['build'] . 'build.config.php';
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 
 $modx= new modX();
 $modx->initialize('mgr');
+echo ''; /* used for nice formatting of log messages */
 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
+$modx->setLogTarget('ECHO');
 
 $modx->loadClass('transport.modPackageBuilder','',false, true);
 $builder = new modPackageBuilder($modx);
 $builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
-// $builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
+$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
 
-/* create category */
-$category = $modx->newObject('modCategory');
+$category= $modx->newObject('modCategory');
 $category->set('id',1);
 $category->set('category',PKG_NAME);
-
-
-/* create the plugin */
-$plugin= $modx->newObject('modPlugin');
-$plugin->set('id',0);
-$plugin->set('name', 'FixTreeSorting');
-$plugin->set('description', 'Keeps resource menuindexes in the correct order.');
-$plugin->set('plugin',file_get_contents($sources['source_core'].'/elements/plugins/fixtreesorting.plugin.php'));
-$category->addMany($plugin);
 
 /* create category vehicle */
 $attr = array(
@@ -87,30 +52,46 @@ $attr = array(
 			xPDOTransport::UPDATE_OBJECT => true,
 			xPDOTransport::UNIQUE_KEY => 'name',
 		),
-	)
+	),
 );
 $vehicle = $builder->createVehicle($category,$attr);
+$builder->putVehicle($vehicle);
+
+
+
+
+
+
+$modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to category...');
+$vehicle->resolve('file',array(
+	'source' => $sources['source_assets'],
+	'target' => "return MODX_ASSETS_PATH . 'components/';",
+));
 $vehicle->resolve('file',array(
 	'source' => $sources['source_core'],
 	'target' => "return MODX_CORE_PATH . 'components/';",
 ));
-$builder->putVehicle($vehicle);
 
-/* now pack in the license file, readme and setup options */
+
+$modx->log(modX::LOG_LEVEL_INFO,'Adding package attributes and setup options...');
 $builder->setPackageAttributes(array(
 	'license' => file_get_contents($sources['docs'] . 'license.txt'),
 	'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
+	'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+	'setup-options' => array(
+		'source' => $sources['build'].'setup.options.php',
+	),
 ));
 
+/* zip up package */
+$modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
 $builder->pack();
 
-$mtime= microtime();
-$mtime= explode(" ", $mtime);
-$mtime= $mtime[1] + $mtime[0];
-$tend= $mtime;
-$totalTime= ($tend - $tstart);
-$totalTime= sprintf("%2.4f s", $totalTime);
-
+$tend= explode(" ", microtime());
+$tend= $tend[1] + $tend[0];
+$totalTime= sprintf("%2.4f s",($tend - $tstart));
 $modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
 
+
+session_write_close();
 exit ();
